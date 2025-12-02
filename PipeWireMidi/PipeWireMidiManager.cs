b7c1f2eda172241725beeafa616ba1e971762adb
Cpaz <nicky.shadow99@gmail.com>
2025-12-01 23:50:56 -0500

    can parse inputs from midi controller

--- PipeWireMidi/PipeWireMidiManager.cs
+++ PipeWireMidi/PipeWireMidiManager.cs
@@ -1,15 +1,25 @@
 ﻿using Commons.Music.Midi;
 using NLog;
-using System.Net.Sockets;
-using System.Text;
 using XVolume.Factory;
-
 namespace PipeWireMidi;
 
 class PipeWireMidiManager {
+
+    private static readonly Logger Logger = BuildLogger();
     
-    private static readonly Logger Logger = new LogFactory().GetCurrentClassLogger();
+    private static Logger BuildLogger() {
+        // TODO: Figure out why config file isn't recognized here 
+        return new LogFactory().Setup().LoadConfiguration(builder => {
+                var logconsole = new NLog.Targets.ConsoleTarget("logconsole"); 
+                builder.Configuration.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            })
+            .GetCurrentClassLogger();
+    }
+
+    private const int MinAnalogValue = 0; // AKA boolean off
     
+    private const int MaxAnalogValue = 127; // AKA boolean on
+
     static void Main(string[] args) {
         var volume = VolumeSubsystemFactory.Create();
 
@@ -28,9 +38,18 @@ class PipeWireMidiManager {
                 Logger.Error("Received unexpected midi input");
                 return;
             }
-            // TODO: Somehow parse data being received here...
-            Logger.Debug(eventArgs);
             
+            // Parse midi message as byte array
+            var midiData = eventArgs.Data.ToArray();
+            if (midiData.Length == 0) {
+                Logger.Error("Received empty midi message");
+                return;
+            }
+            
+            // TODO: Open source midi device mappings to distinguish between "buttons" vs analog "sliders" or "knobs"
+            byte input = midiData[0];
+            byte inputValue = midiData[1];
+            Logger.Debug($"Unkown input: Input: {input}, Value: {inputValue}");
         };
 
         Logger.Info("Listening for MIDI input... Press any key to exit.");
