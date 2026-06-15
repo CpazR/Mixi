9b298018180472cce5b4bcd8550e53e07b47ee52
Cpaz <nicky.shadow99@gmail.com>
2026-06-14 20:53:07 -0400

    cleaned up dependencies and updated flatpak build workflow

--- artifacts/scripts/build-flatpak.sh
+++ artifacts/scripts/build-flatpak.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python3 flatpak-dotnet-generator.py mixi-sources.json ../../Mixi/Mixi.csproj --runtime linux-x64 --dotnet-args --no-cache --verbosity detailed
+python3 flatpak-dotnet-generator.py ../flatpak/mixi-sources.json ../../Mixi/Mixi.csproj --runtime linux-x64 --dotnet-args --no-cache --verbosity detailed
 flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install builddir ../flatpak/cpaz.flatpak.Mixi.yml
